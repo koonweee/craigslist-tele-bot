a61741f2a04cf968ea5471b8d972b63e26e12918
Jeremy Tan <jeremytkw98@gmail.com>
2022-01-28 01:50:22 -0800

    Users database functionality added. Pending database for previously seen listings

--- config.py
+++
@@ -1,2 +0,0 @@
-telegramAPIKey = "5212481231:AAFwdqGuMJa72TbpitX2QlvkzUnMAgv8P7w"
-craigslistURL = "https://sfbay.craigslist.org/search/cta?lat=37.6760&lon=-122.4760&postedToday=1&search_distance=200&min_price=2200&max_price=8500&max_auto_miles=175%2C000&auto_title_status=1&auto_transmission=2"
