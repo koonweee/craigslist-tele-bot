15cc5b69868de4afd7dc9df7801bd4957ab86b49
Jeremy Tan <jeremytkw98@gmail.com>
2022-01-28 16:05:29 -0800

    Add sample config.py

---
+++ sample_config.py
@@ -0,0 +1,4 @@
+# this is a SAMPLE config file, please update the values and rename it to config.py
+# it should be placed in the same directory as the other .py files (main.py etc)
+
+telegramAPIKey = <insert your telegram bot api key here>
